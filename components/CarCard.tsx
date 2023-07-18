8ec85c243c97bc39a62619be362957f1ddf77fdf
Veysel Kartalmis <veyselkartalms@gmail.com>
2023-07-18 15:23:40 +0300

    card details modal added

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { CarProps } from "@/types";
 import CustomButton from "./CustomButton";
 import { calculateCarRent } from "@/utils";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
     car: CarProps;
@@ -25,13 +26,11 @@ const CarCard = ({ car }: CarCardProps) => {
                     {make} {model}
                 </h2>
             </div>
-
             <p className="flex mt-6 text-[32px] font-extrabold">
                 <span className="self-start text-[14px] font-semibold">$</span>
                 {carRent}
                 <span className="self-end text-[14px] font-medium">/day</span>
             </p>
-
             <div className="relative w-full h-40 my-3 object-contain">
                 <Image
                     src="/hero.png"
@@ -41,7 +40,6 @@ const CarCard = ({ car }: CarCardProps) => {
                     className="object-contain"
                 />
             </div>
-
             <div className="relative flex w-full mt-2">
                 <div className="flex group-hover:invisible w-full justify-between text-gray">
                     <div className="flex flex-col justify-center items-center gap-2">
@@ -85,6 +83,11 @@ const CarCard = ({ car }: CarCardProps) => {
                     />
                 </div>
             </div>
+            <CarDetails
+                isOpen={isOpen}
+                closeModal={() => setIsOpen(false)}
+                car={car}
+            />
         </div>
     );
 };
